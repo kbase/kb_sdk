385966d698c4de37847d5d37811967512c882bb5
Roman Sutormin <rsutormin@lbl.gov>
2016-03-02 16:34:38 -0800

    Version is increased to 1.0.1

--- src/java/us/kbase/mobu/ModuleBuilder.java
+++ src/java/us/kbase/mobu/ModuleBuilder.java
@@ -36,7 +36,7 @@ public class ModuleBuilder {
     
     public static final String DEFAULT_METHOD_STORE_URL = "https://appdev.kbase.us/services/narrative_method_store/rpc";
     
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     
     
     public static void main(String[] args) throws Exception {
