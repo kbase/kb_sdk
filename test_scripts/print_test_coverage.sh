2f3113125003cab0af5a0f60dba3030c6cb21f62
Roman Sutormin <rsutormin@lbl.gov>
2016-10-07 10:30:49 -0700

    More convenient way of saving test coverage report

--- test_scripts/print_test_coverage.sh
+++ test_scripts/print_test_coverage.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "Code coverage:"
 cat ./reports/coverage-report.csv | \
 grep "us.kbase" | grep -v "{...}" | \
 grep -v "us.kbase.narrativemethodstore" | \
 grep -v "us.kbase.catalog" | \
 awk -F ',' '{ cov+=$5; all+=$4+$5; print 100*$5/($4+$5)"\t"$2"."$3, "(covered="$5"/"($4+$5)")" } END { print "Common coverage: "(100*cov/all)"("cov"/"all")" }' | \
 grep -v ".test." | \
-sort -k1,1nr
+sort -k1,1nr > ./reports/coverage-report-nice.txt
+echo -e "\nCode coverage:"
+cat ./reports/coverage-report-nice.txt
