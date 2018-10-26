67862442c1d82278a318a2d69765bb92f04fe655
JamesJeffryes <jamesgjeffryes@gmail.com>
2018-10-26 09:04:31 -0700

    Installed clients in perl, r and python go into a installed_clients folder

--- src/java/us/kbase/mobu/installer/ClientInstaller.java
+++ src/java/us/kbase/mobu/installer/ClientInstaller.java
@@ -291,16 +291,16 @@ public class ClientInstaller {
         } else {
             String perlClientName = null;
             if (isPerl)
-                perlClientName = moduleName + "::" + clientName + "Client";
+                perlClientName = "installed_clients::" + clientName + "Client";
             String pyClientName = null;
             if (isPython)
-                pyClientName = moduleName + "." + clientName + "Client";
+                pyClientName = "installed_clients." + clientName + "Client";
             String rClientName = null;
             if (isR)
-                rClientName = moduleName + "/" + clientName + "Client";
+                rClientName = "installed_clients/" + clientName + "Client";
             String jsClientName = null;
             if (isJS)
-                jsClientName = moduleName + "/" + clientName + "Client";
+                jsClientName = "installed_clients/" + clientName + "Client";
             FileSaver output = new DiskFileSaver(libDir);
             TemplateBasedGenerator.generate(services, url, isJS, jsClientName, isPerl, 
                     perlClientName, false, null, null, null, isPython, pyClientName, false, null,
