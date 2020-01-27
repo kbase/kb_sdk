b847809b13a4062b87cf61131b136232d2562c22
ialarmedalien <ialarmedalien@gmail.com>
2020-01-27 14:13:32 -0800

    Simplifying export declaration and adding strict/warnings

--- submodules_hacks/AuthConstants.pm
+++ submodules_hacks/AuthConstants.pm
@@ -1,22 +1,34 @@
 package Bio::KBase::AuthConstants;
 
-use constant        globus_token_url   => 'https://nexus.api.globusonline.org/goauth/token?grant_type=client_credentials';
-use constant        globus_profile_url   => 'https://nexus.api.globusonline.org/users';
-use constant	    trust_token_signers => split(/\s+/, 'https://nexus.api.globusonline.org/goauth/keys');
-use constant	    role_service_url => 'https://kbase.us/services/authorization/Roles';
+use strict;
+use warnings;
 
 use base 'Exporter';
-our @EXPORT_OK = qw(globus_token_url globus_profile_url trust_token_signers role_service_url);
-our %EXPORT_TAGS = ( 
-		    globus => [ qw(globus_token_url globus_profile_url trust_token_signers) ],
-		    kbase  => [ qw(role_service_url) ],
-		   );
-{
-    my %seen;
-    
-    push @{$EXPORT_TAGS{all}},
-    grep {!$seen{$_}++} @{$EXPORT_TAGS{$_}} foreach keys %EXPORT_TAGS;
-}
+
+use constant {
+    globus_token_url    =>
+        'https://nexus.api.globusonline.org/goauth/token?grant_type=client_credentials',
+
+    globus_profile_url  => 'https://nexus.api.globusonline.org/users',
+
+    role_service_url    => 'https://kbase.us/services/authorization/Roles',
+};
+
+use constant trust_token_signers =>
+        split /\s+/, 'https://nexus.api.globusonline.org/goauth/keys';
+
+our @EXPORT_OK = qw(
+    globus_profile_url
+    globus_token_url
+    role_service_url
+    trust_token_signers
+);
+
+our %EXPORT_TAGS = (
+    globus  => [ qw( globus_token_url globus_profile_url trust_token_signers ) ],
+    kbase   => [ qw( role_service_url ) ],
+    all     => [ @EXPORT_OK ],
+);
 
 1;
 
